307183f6e2919fd5bdc38dae034d40fb3dd0a685
humbertoragazzini <beto_vicio@hotmail.com>
2025-11-21 22:37:35 +0000

    added new design to the home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import MyCarousel from "../components/organisms/Carousel";
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen py-16">
+    <main className="w-full min-h-screen pb-16">
       {/* Header */}
-      <div className="mx-auto h-[calc(100vh-160px)] max-w-[1480px] flex justify-center items-center">
-        <div className="">
+      <div className="mx-auto h-[calc(100vh-160px)] w-full flex justify-center items-center">
+        <div className="max-w-[1480px]">
           <Heading
             level={1}
             className="text-white font-bold text-center mb-4 text-[100px]"
@@ -42,59 +42,59 @@ export default function Home() {
         </div>
       </div>
       {/* carousel */}
-      <div className="w-full mb-20">
+      <div className="w-full min-h-screen flex flex-col justify-center items-center py-16 bg-slate-950 mb-40">
         <Heading level={2} className="text-center font-bold text-white">
           Some heading for the carousel
         </Heading>
-      </div>
-      <div className="w-full bg-slate-900 mb-40">
         <MyCarousel></MyCarousel>
       </div>
       {/* cards */}
-      <div className="mx-auto max-w-[1480px] text-white mb-[200px]">
-        <div className="grid grid-cols-12">
-          <div className="col-span-4">
-            <Heading level={2} className="">
-              Made for mass adoption.
-            </Heading>
-          </div>
-          <div className="col-span-8">
-            <div className="grid grid-cols-2 gap-7">
-              <div className="col-span-1 border-1 rounded-2xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl px-8 py-6 translate-y-[80px] shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
-                <Heading level={3}>Decentralized</Heading>
-                <p>
-                  The Solana network is validated by thousands of nodes that
-                  operate independently of each other, ensuring your data
-                  remains secure and censorship resistant.
-                </p>
-                <p>850</p>
-              </div>
-              <div className="col-span-1 border-1 rounded-2xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl px-8 py-6 shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
-                <Heading level={3}>Decentralized</Heading>
-                <p>
-                  The Solana network is validated by thousands of nodes that
-                  operate independently of each other, ensuring your data
-                  remains secure and censorship resistant.
-                </p>
-                <p>850</p>
-              </div>
-              <div className="col-span-1 border-1 rounded-2xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl px-8 py-6 translate-y-[80px] shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
-                <Heading level={3}>Decentralized</Heading>
-                <p>
-                  The Solana network is validated by thousands of nodes that
-                  operate independently of each other, ensuring your data
-                  remains secure and censorship resistant.
-                </p>
-                <p>850</p>
-              </div>
-              <div className="col-span-1 border-1 rounded-2xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl px-8 py-6 shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
-                <Heading level={3}>Decentralized</Heading>
-                <p>
-                  The Solana network is validated by thousands of nodes that
-                  operate independently of each other, ensuring your data
-                  remains secure and censorship resistant.
-                </p>
-                <p>850</p>
+      <div className=" w-full min-h-screen flex justify-center items-center">
+        <div className="mx-auto max-w-[1480px] text-white mb-[200px]">
+          <div className="grid grid-cols-12">
+            <div className="col-span-4">
+              <Heading level={2} className="">
+                Made for mass adoption.
+              </Heading>
+            </div>
+            <div className="col-span-8">
+              <div className="grid grid-cols-2 gap-7">
+                <div className="col-span-1 border-1 rounded-2xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl px-8 py-6 translate-y-[80px] shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                  <Heading level={3}>Decentralized</Heading>
+                  <p>
+                    The Solana network is validated by thousands of nodes that
+                    operate independently of each other, ensuring your data
+                    remains secure and censorship resistant.
+                  </p>
+                  <p>850</p>
+                </div>
+                <div className="col-span-1 border-1 rounded-2xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl px-8 py-6 shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                  <Heading level={3}>Decentralized</Heading>
+                  <p>
+                    The Solana network is validated by thousands of nodes that
+                    operate independently of each other, ensuring your data
+                    remains secure and censorship resistant.
+                  </p>
+                  <p>850</p>
+                </div>
+                <div className="col-span-1 border-1 rounded-2xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl px-8 py-6 translate-y-[80px] shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                  <Heading level={3}>Decentralized</Heading>
+                  <p>
+                    The Solana network is validated by thousands of nodes that
+                    operate independently of each other, ensuring your data
+                    remains secure and censorship resistant.
+                  </p>
+                  <p>850</p>
+                </div>
+                <div className="col-span-1 border-1 rounded-2xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl px-8 py-6 shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                  <Heading level={3}>Decentralized</Heading>
+                  <p>
+                    The Solana network is validated by thousands of nodes that
+                    operate independently of each other, ensuring your data
+                    remains secure and censorship resistant.
+                  </p>
+                  <p>850</p>
+                </div>
               </div>
             </div>
           </div>
