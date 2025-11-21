3494c9bd7dae6152aec10147f8d2a55b69288ae0
humbertoragazzini <beto_vicio@hotmail.com>
2025-11-21 22:17:34 +0000

    starting new design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,26 +8,28 @@ export default function Home() {
   return (
     <main className="w-full min-h-screen py-16">
       {/* Header */}
-      <div className="mx-auto max-w-6xl px-6 py-10 h-full backdrop-blur-lg border-1 rounded-4xl border-[rgba(255,255,255,0.1)] mb-40 shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
-        <Heading level={1} className="text-white font-bold text-center mb-4">
-          <span>
-            Powerful for developers.<br></br> Fast for everyone.
-          </span>
-        </Heading>
-        <p className="font-semi text-xl text-white text-center mx-auto w-[75%] mb-8">
-          Bring blockchain to the people. Solana supports experiences for power
-          users, new consumers, and everyone in between.
-        </p>
-        <div className="flex justify-center items-center mb-8">
-          <div>
-            <PpButton theme={"yellow"} icon={<FaBeer size={18} />} href={"/"}>
-              Contact Us
-            </PpButton>
-          </div>
-          <div className="ml-3">
-            <PpButton theme={"yellow"} icon={<FaBeer size={18} />} href={"/"}>
-              Check our services
-            </PpButton>
+      <div className="mx-auto h-[calc(100vh-160px)] max-w-6xl flex justify-center items-center">
+        <div className="">
+          <Heading level={1} className="text-white font-bold text-center mb-4">
+            <span>
+              Powerful for developers.<br></br> Fast for everyone.
+            </span>
+          </Heading>
+          <p className="font-semi text-xl text-white text-center mx-auto w-[75%] mb-8">
+            Bring blockchain to the people. Solana supports experiences for
+            power users, new consumers, and everyone in between.
+          </p>
+          <div className="flex justify-center items-center mb-8">
+            <div>
+              <PpButton theme={"yellow"} icon={<FaBeer size={18} />} href={"/"}>
+                Contact Us
+              </PpButton>
+            </div>
+            <div className="ml-3">
+              <PpButton theme={"yellow"} icon={<FaBeer size={18} />} href={"/"}>
+                Check our services
+              </PpButton>
+            </div>
           </div>
         </div>
       </div>
