71a8894ba36e52c824c8d91e0ecaf375c1e9b0c4
humbertoragazzini <beto_vicio@hotmail.com>
2025-11-22 23:44:24 +0000

    added new cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
             </div>
             <div className="col-span-8">
               <div className="grid grid-cols-2 gap-7">
-                <div className="col-span-1 border-1 aspect-[4/3] rounded-3xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl px-8 py-7 translate-y-[80px] shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                <div className="col-span-1 translate-y-32 rounded-3xl backdrop-blur-2xl shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
                   {/* <div className="w-16 h-16 mb-4 rounded-xl bg-slate-800 flex justify-center items-center">
                     <BiBeer className="w-6 h-6"></BiBeer>
                   </div> */}
@@ -82,7 +82,67 @@ export default function Home() {
                   </p> */}
                   <Example></Example>
                 </div>
-                <div className="col-span-1 border-1 aspect-[4/3] rounded-3xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl px-8 py-7 shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                <div className="col-span-1 rounded-3xl backdrop-blur-2xl shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                  {/* <div className="w-16 h-16 mb-4 rounded-xl bg-slate-800 flex justify-center items-center">
+                    <BiBeer className="w-6 h-6"></BiBeer>
+                  </div> */}
+                  {/* <div
+                    className="w-full mb-5 rounded-xl bg-cover bg-bottoms flex justify-center items-center aspect-3/1 overflow-hidden"
+                    style={{ backgroundImage: "url(./images/ship.avif)" }}
+                  ></div>
+                  <Heading level={3} className="mb-4 text-md font-semibold">
+                    <MultiLangSpan
+                      content={homeContent.solutions.cardSmartImportTitle}
+                    ></MultiLangSpan>
+                  </Heading>
+                  <p className="mb-4 text-muted-steel-blue">
+                    <MultiLangSpan
+                      content={homeContent.solutions.cardSmartImportBody}
+                    ></MultiLangSpan>
+                  </p> */}
+                  <Example></Example>
+                </div>
+                <div className="col-span-1 translate-y-32 rounded-3xl backdrop-blur-2xl shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                  {/* <div className="w-16 h-16 mb-4 rounded-xl bg-slate-800 flex justify-center items-center">
+                    <BiBeer className="w-6 h-6"></BiBeer>
+                  </div> */}
+                  {/* <div
+                    className="w-full mb-5 rounded-xl bg-cover bg-bottoms flex justify-center items-center aspect-3/1 overflow-hidden"
+                    style={{ backgroundImage: "url(./images/ship.avif)" }}
+                  ></div>
+                  <Heading level={3} className="mb-4 text-md font-semibold">
+                    <MultiLangSpan
+                      content={homeContent.solutions.cardSmartImportTitle}
+                    ></MultiLangSpan>
+                  </Heading>
+                  <p className="mb-4 text-muted-steel-blue">
+                    <MultiLangSpan
+                      content={homeContent.solutions.cardSmartImportBody}
+                    ></MultiLangSpan>
+                  </p> */}
+                  <Example></Example>
+                </div>
+                <div className="col-span-1 rounded-3xl backdrop-blur-2xl shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                  {/* <div className="w-16 h-16 mb-4 rounded-xl bg-slate-800 flex justify-center items-center">
+                    <BiBeer className="w-6 h-6"></BiBeer>
+                  </div> */}
+                  {/* <div
+                    className="w-full mb-5 rounded-xl bg-cover bg-bottoms flex justify-center items-center aspect-3/1 overflow-hidden"
+                    style={{ backgroundImage: "url(./images/ship.avif)" }}
+                  ></div>
+                  <Heading level={3} className="mb-4 text-md font-semibold">
+                    <MultiLangSpan
+                      content={homeContent.solutions.cardSmartImportTitle}
+                    ></MultiLangSpan>
+                  </Heading>
+                  <p className="mb-4 text-muted-steel-blue">
+                    <MultiLangSpan
+                      content={homeContent.solutions.cardSmartImportBody}
+                    ></MultiLangSpan>
+                  </p> */}
+                  <Example></Example>
+                </div>
+                {/* <div className="col-span-1 border-1 aspect-[4/3] rounded-3xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl px-8 py-7 shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
                   <div className="w-16 h-16 mb-4 rounded-xl bg-slate-800 flex justify-center items-center">
                     <BiBeer className="w-6 h-6"></BiBeer>
                   </div>
@@ -126,7 +186,7 @@ export default function Home() {
                       content={homeContent.solutions.cardLogisticsBody}
                     ></MultiLangSpan>
                   </p>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
--- src/components/atoms/Card.tsx
+++ src/components/atoms/Card.tsx
@@ -1,18 +1,11 @@
+"use client";
 import { FiCloudLightning } from "react-icons/fi";
 import { motion } from "framer-motion";
 
-const Example = () => {
-  return (
-    <div className="bg-slate-950 px-4 py-12">
-      <ShimmerBorderCard />
-    </div>
-  );
-};
-
 const ShimmerBorderCard = () => {
   return (
-    <div className="group relative mx-auto w-full max-w-sm overflow-hidden rounded-lg bg-slate-800 p-0.5 transition-all duration-500 hover:scale-[1.01] hover:bg-slate-800/50">
-      <div className="relative z-10 flex flex-col items-center justify-center overflow-hidden rounded-[7px] bg-slate-900 p-8 transition-colors duration-500 group-hover:bg-slate-800">
+    <div className="group relative mx-auto w-full overflow-hidden rounded-3xl bg-slate-800 p-0.5 transition-all duration-500 hover:scale-[1.01] hover:bg-slate-800/50">
+      <div className="relative z-10 flex flex-col items-center justify-center overflow-hidden rounded-3xl bg-slate-900 p-8 transition-colors duration-500 group-hover:bg-slate-800">
         <FiCloudLightning className="relative z-10 mb-10 mt-2 rounded-full border-2 border-indigo-500 bg-slate-900 p-4 text-7xl text-indigo-500" />
 
         <h4 className="relative z-10 mb-4 w-full text-3xl font-bold text-slate-50">
@@ -41,4 +34,4 @@ const ShimmerBorderCard = () => {
   );
 };
 
-export default Example;
+export default ShimmerBorderCard;
