6c762b6fe8717e752ee1975a8aa8eb9d6c0c90b5
humbertoragazzini <beto_vicio@hotmail.com>
2025-11-25 19:30:49 +0000

    starting with timeline section

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -82,25 +82,28 @@ export default function AboutUs() {
         </div>
       </div>
 
-      {/* our mission */}
+      {/* our values */}
       <div id="home-highlights" className="w-full mb-40">
         <div className=" inset-0 py-16 h-full">
           <div className="w-full mx-auto max-w-[1280px] px-2 xl:px-6 text-white">
             <Heading level={2} className="text-center font-semibold! mb-8">
               <MultiLangSpan
-                content={aboutUsContent.values.title}
+                content={aboutUsContent.story.title}
               ></MultiLangSpan>
             </Heading>
             <div className="mb-16 lg:max-w-[75%] mx-auto">
               <p className="text-center text-xl! lg:text-2xl! text-muted-steel-blue">
                 <MultiLangSpan
-                  content={aboutUsContent.values.subtitle}
+                  content={aboutUsContent.story.subtitle}
                 ></MultiLangSpan>
               </p>
             </div>
             <div className="grid grid-cols-2 gap-8 mx-auto">
               <div className="col-span-1">
                 <div className="border-1 p-6 rounded-2xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl overflow-hidden shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                  <div className="w-16 h-16 flex justify-center items-center bg bg-slate-950 rounded-full border-slate-600 border-2 mb-6">
+                    <FaBeer className="w-6 h-6"></FaBeer>
+                  </div>
                   <p className="font-semibold text-xl mb-4">
                     <MultiLangSpan
                       content={aboutUsContent.values.items[0].title}
@@ -115,6 +118,9 @@ export default function AboutUs() {
               </div>
               <div className="col-span-1">
                 <div className="border-1 p-6 rounded-2xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl overflow-hidden shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                  <div className="w-16 h-16 flex justify-center items-center bg bg-slate-950 rounded-full border-slate-600 border-2 mb-6">
+                    <FaBeer className="w-6 h-6"></FaBeer>
+                  </div>
                   <p className="font-semibold text-xl mb-4">
                     <MultiLangSpan
                       content={aboutUsContent.values.items[1].title}
@@ -129,6 +135,9 @@ export default function AboutUs() {
               </div>
               <div className="col-span-1">
                 <div className="border-1 p-6 rounded-2xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl overflow-hidden shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                  <div className="w-16 h-16 flex justify-center items-center bg bg-slate-950 rounded-full border-slate-600 border-2 mb-6">
+                    <FaBeer className="w-6 h-6"></FaBeer>
+                  </div>
                   <p className="font-semibold text-xl mb-4">
                     <MultiLangSpan
                       content={aboutUsContent.values.items[2].title}
@@ -143,6 +152,9 @@ export default function AboutUs() {
               </div>
               <div className="col-span-1">
                 <div className="border-1 p-6 rounded-2xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl overflow-hidden shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                  <div className="w-16 h-16 flex justify-center items-center bg bg-slate-950 rounded-full border-slate-600 border-2 mb-6">
+                    <FaBeer className="w-6 h-6"></FaBeer>
+                  </div>
                   <p className="font-semibold text-xl mb-4">
                     <MultiLangSpan
                       content={aboutUsContent.values.items[3].title}
@@ -151,6 +163,59 @@ export default function AboutUs() {
                   <p>
                     <MultiLangSpan
                       content={aboutUsContent.values.items[3].body}
+                    ></MultiLangSpan>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* our mission */}
+      <div
+        id="home-highlights"
+        className="w-full mb-40 py-[1px] bg-[radial-gradient(circle,rgba(2,6,24,1)_0%,rgba(87,199,133,1)_0%,rgba(2,6,24,1)_100%)]"
+      >
+        <div className=" inset-0 bg-gradient-to-br from-slate-800 via-slate-950 to-slate-800 py-16 h-full">
+          <div className="w-full mx-auto max-w-[1480px] px-2 xl:px-6 text-white">
+            <Heading level={2} className="text-left font-semibold! mb-8">
+              <MultiLangSpan
+                content={aboutUsContent.mission.title}
+              ></MultiLangSpan>
+            </Heading>
+            <div className="mb-16 lg:max-w-[75%] mx-auto">
+              <p className="text-xl! lg:text-2xl! text-muted-steel-blue">
+                <MultiLangSpan
+                  content={aboutUsContent.mission.body}
+                ></MultiLangSpan>
+              </p>
+            </div>
+            <div className="grid grid-cols-2 gap-8 lg:max-w-[75%] mx-auto">
+              <div className="col-span-1">
+                <div className="border-1 p-6 rounded-2xl bg-slate-800 border-[rgba(255,255,255,0.1)] backdrop-blur-2xl overflow-hidden shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                  <p className="font-semibold text-xl mb-4">
+                    <MultiLangSpan
+                      content={aboutUsContent.mission.cards[0].title}
+                    ></MultiLangSpan>
+                  </p>
+                  <p>
+                    <MultiLangSpan
+                      content={aboutUsContent.mission.cards[0].body}
+                    ></MultiLangSpan>
+                  </p>
+                </div>
+              </div>
+              <div className="col-span-1">
+                <div className="border-1 p-6 rounded-2xl bg-slate-800 border-[rgba(255,255,255,0.1)] backdrop-blur-2xl overflow-hidden shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                  <p className="font-semibold text-xl mb-4">
+                    <MultiLangSpan
+                      content={aboutUsContent.mission.cards[1].title}
+                    ></MultiLangSpan>
+                  </p>
+                  <p>
+                    <MultiLangSpan
+                      content={aboutUsContent.mission.cards[1].body}
                     ></MultiLangSpan>
                   </p>
                 </div>
