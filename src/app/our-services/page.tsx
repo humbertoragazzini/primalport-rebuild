f51508afd4311e8b66bceb78e1b4fccd36eab175
humbertoragazzini <beto_vicio@hotmail.com>
2025-11-26 22:29:36 +0000

    adding process

--- src/app/our-services/page.tsx
+++ src/app/our-services/page.tsx
@@ -210,7 +210,7 @@ export default function Home() {
             <div className="mb-4 lg:max-w-[75%] mx-auto">
               <Heading level={2} className="text-center font-semibold! mb-8">
                 <MultiLangSpan
-                  content={servicesPageContent.consultingSection.title}
+                  content={servicesPageContent.paperSection.title}
                 ></MultiLangSpan>
               </Heading>
             </div>
@@ -249,6 +249,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+
       {/* our values */}
       <div id="home-highlights" className="w-full mb-40">
         <div className=" inset-0 py-16 h-full">
@@ -265,31 +266,65 @@ export default function Home() {
                 ></MultiLangSpan>
               </p>
             </div>
-            {/* <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mx-auto">
-              {servicesPageContent.team.members.map((m) => {
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mx-auto">
+              {servicesPageContent.whyUs.points.map((m) => {
                 return (
                   <div className="col-span-1">
                     <div className="border-1 p-6 rounded-2xl bg-slate-700/40 border-[rgba(255,255,255,0.1)] backdrop-blur-2xl overflow-hidden shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
-                      <div className="aspect-square w-fit overflow-hidden mx-auto p-3 flex justify-center items-center bg bg-slate-950 rounded-full border-slate-600 border-2 mb-6">
+                      {/* <div className="aspect-square w-fit overflow-hidden mx-auto p-3 flex justify-center items-center bg bg-slate-950 rounded-full border-slate-600 border-2 mb-6">
                         <img
                           className="max-w-40 rounded-full w-full aspect-square"
                           src="http://placehold.co/450x450"
                         ></img>
-                      </div>
-                      <p className="font-semibold text-xl mb-4 text-yellow-bright">
-                        <MultiLangSpan content={m.name}></MultiLangSpan>
-                      </p>
-                      <p className="mb-4">
-                        <MultiLangSpan content={m.bio}></MultiLangSpan>
-                      </p>
-                      <p className="text-sm">
-                        <MultiLangSpan content={m.role}></MultiLangSpan>
+                      </div> */}
+                      <p className="font-semibold text-xl text-yellow-bright">
+                        <MultiLangSpan content={m}></MultiLangSpan>
                       </p>
                     </div>
                   </div>
                 );
               })}
-            </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* our mission */}
+      <div
+        id="home-highlights"
+        className="w-full mb-40 py-[1px] bg-[radial-gradient(circle,rgba(2,6,24,1)_0%,rgba(87,199,133,1)_0%,rgba(2,6,24,1)_100%)]"
+      >
+        <div className=" inset-0 bg-gradient-to-br from-slate-800 via-slate-950 to-slate-800 py-16 h-full">
+          <div className="w-full mx-auto max-w-[1480px] px-2 xl:px-6 text-white">
+            <div className="mb-4 lg:max-w-[75%] mx-auto">
+              <Heading level={2} className="text-center font-semibold! mb-8">
+                <MultiLangSpan
+                  content={servicesPageContent.process.title}
+                ></MultiLangSpan>
+              </Heading>
+            </div>
+
+            <div className="grid lg:max-w-[1200px] grid-cols-1 md:grid-cols-2 gap-8 mx-auto">
+              {servicesPageContent.process.steps.map((b) => {
+                return (
+                  <div className="col-span-1 relative">
+                    <div className="border-1 p-6 flex items-center bg-slate-700 rounded-2xl border-[rgba(255,255,255,0.1)] backdrop-blur-2xl overflow-hidden shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
+                      <div className="w-[28px] h-[28px] p-[2px] bg bg-slate-950 rounded-full border-yellow-bright border-2">
+                        {b.number}
+                      </div>
+                      <div>
+                        <p className="text-xl font-semibold ml-5">
+                          <MultiLangSpan content={b.title}></MultiLangSpan>
+                        </p>
+                        <p className="text-md ml-5">
+                          <MultiLangSpan content={b.body}></MultiLangSpan>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
