a5c34886dd5541a5706d6537961dd1826ee2ad37
humbertoragazzini <beto_vicio@hotmail.com>
2025-11-27 23:32:12 +0000

    all headers changed

--- src/app/our-services/page.tsx
+++ src/app/our-services/page.tsx
@@ -6,34 +6,17 @@ import MyCarousel from "../../components/organisms/Carousel";
 import MultiLangSpan from "@/src/components/atoms/MultiLangSpan";
 import { FaCircleCheck } from "react-icons/fa6";
 import servicesPageContent from "@/src/data/servicesPageContent";
+import HeadingSection from "@/src/components/organisms/Header";
 
 export default function OurServices() {
   return (
     <main className="w-full min-h-screen pb-16" id="home-hero">
       {/* Header */}
-      <div className="mx-auto px-2 xl:px-6 py-16 lg:py-0 h-screen lg:h-[calc(100vh-160px)] w-full flex justify-center items-center">
-        <div className="max-w-[1480px]">
-          <div className="rounded-full border-yellow-bright border-2 text-white py-4 px-6 w-fit mx-auto font-semibold mb-4 text-2xl">
-            <MultiLangSpan
-              content={servicesPageContent.hero.tag}
-            ></MultiLangSpan>
-          </div>
-          <Heading
-            level={1}
-            className="text-white font-bold text-center mb-4 xl:text-[100px]!"
-          >
-            <MultiLangSpan
-              content={servicesPageContent.hero.title}
-            ></MultiLangSpan>
-          </Heading>
-          <p className="font-semi text-lg! lg:text-xl! text-muted-steel-blue text-center mx-auto xl:w-[75%] mb-8">
-            <MultiLangSpan
-              content={servicesPageContent.hero.subtitle}
-            ></MultiLangSpan>
-          </p>
-          <div className="flex justify-center items-center mb-8"></div>
-        </div>
-      </div>
+      <HeadingSection
+        tag={servicesPageContent.hero.tag}
+        title={servicesPageContent.hero.title}
+        subtitle={servicesPageContent.hero.subtitle}
+      ></HeadingSection>
 
       {/* our mission */}
       <div
