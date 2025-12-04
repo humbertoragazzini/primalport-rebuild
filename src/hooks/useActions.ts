a71df0b69987804e9c3e9199acde531c7f93cc3b
humbertoragazzini <beto_vicio@hotmail.com>
2025-12-04 21:38:11 +0000

    building the container for the tool

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -3,4 +3,6 @@ import { create } from "zustand";
 export const useActions = create((set) => ({
   menu: false,
   setMenuToggle: (open: boolean) => set({ menu: !open }),
+  card: "",
+  setCardToggle: (value: string) => set({ card: value }),
 }));
