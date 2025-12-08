2aa6ceb31f4be3d8c51f48fa2558ab52da8e38c3
humbertoragazzini <beto_vicio@hotmail.com>
2025-12-08 18:55:33 +0000

    added some style to the cards

--- src/components/organisms/tools/Card.tsx
+++ src/components/organisms/tools/Card.tsx
@@ -22,14 +22,17 @@ export default function Card({ title, body, icon, id }: iCard) {
         className="group cursor-pointer relative mx-auto w-full h-full overflow-hidden rounded-3xl bg-slate-800 p-0.5 transition-all duration-500 hover:scale-[1.01] hover:bg-slate-800/50"
       >
         <div className="relative h-full z-10 flex flex-col items-center justify-center overflow-hidden rounded-3xl bg-slate-900 p-8 transition-colors duration-500 group-hover:bg-slate-800">
-          <div className="main-container">
+          <div className="main-container h-full flex flex-col justify-between items-center">
             <div className="icon-container mx-auto w-28 h-28 z-10 mb-10 mt-2 rounded-full border-2 border-indigo-500 bg-slate-900 flex justify-center items-center text-7xl">
               {icon}
             </div>
+
             <h4 className="title relative z-10 mb-4 w-full text-3xl font-bold text-slate-50">
               {title}
             </h4>
-            <p className="body relative z-10 text-slate-400">{body}</p>
+            <div>
+              <p className="body relative z-10 text-slate-400">{body}</p>
+            </div>
           </div>
         </div>
 
