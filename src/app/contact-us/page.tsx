3ebb223ea11ac74cf4ba46497a1c378ae16ca272
humbertoragazzini <beto_vicio@hotmail.com>
2025-12-09 21:57:24 +0000

    added de aanimations to the contact page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -16,6 +16,7 @@ import { RiMapPin2Fill } from "react-icons/ri";
 import { FiPhone } from "react-icons/fi";
 import { LuMail } from "react-icons/lu";
 import { TbClock } from "react-icons/tb";
+import { Direction, ScrollReveal } from "@/src/components/atoms/ScrollReveal";
 
 export default function Contact() {
   return (
@@ -31,25 +32,37 @@ export default function Contact() {
       <GradientSection>
         <>
           <div className="w-full mx-auto max-w-[1480px]">
-            <Heading
-              level={2}
-              className="text-center font-semibold! text-white"
-            >
-              <MultiLangSpan
-                content={contactPageContent.infoSection.title}
-              ></MultiLangSpan>
-            </Heading>
+            <ScrollReveal direction={"left"} delay={Math.random()}>
+              <Heading
+                level={2}
+                className="text-center font-semibold! text-white"
+              >
+                <MultiLangSpan
+                  content={contactPageContent.infoSection.title}
+                ></MultiLangSpan>
+              </Heading>
+            </ScrollReveal>
           </div>
           <div className="mb-16 lg:max-w-[1280px] mx-auto">
-            <p className="text-md! text-center lg:text-2xl! text-muted-steel-blue">
-              <MultiLangSpan
-                content={contactPageContent.infoSection.subtitle}
-              ></MultiLangSpan>
-            </p>
+            <ScrollReveal direction={"left"} delay={Math.random()}>
+              <p className="text-md! text-center lg:text-2xl! text-muted-steel-blue">
+                <MultiLangSpan
+                  content={contactPageContent.infoSection.subtitle}
+                ></MultiLangSpan>
+              </p>
+            </ScrollReveal>
           </div>
           <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-6 ">
-            {contactPageContent.infoSection.cards.map((c) => {
+            {contactPageContent.infoSection.cards.map((c, i) => {
               let icon = null;
+              const from = [
+                "left",
+                "up",
+                "bottom",
+                "right",
+                "bottom",
+                "bottom",
+              ];
               if (c.icon == "location") {
                 icon = (
                   <RiMapPin2Fill className="w-14 h-14 relative text-indigo-500" />
@@ -71,24 +84,29 @@ export default function Contact() {
                 );
               }
               return (
-                <ShimmerBorderCard
-                  title={
-                    <div className="text-center w-full">
-                      <MultiLangSpan content={c.title}></MultiLangSpan>
-                    </div>
-                  }
-                  body={
-                    <>
-                      <div className="w-full text-center">
-                        <MultiLangSpan content={c.line1}></MultiLangSpan>
+                <ScrollReveal
+                  direction={from[i] as Direction}
+                  delay={Math.random()}
+                >
+                  <ShimmerBorderCard
+                    title={
+                      <div className="text-center w-full">
+                        <MultiLangSpan content={c.title}></MultiLangSpan>
                       </div>
-                      <div className="w-full text-center">
-                        <MultiLangSpan content={c.line2}></MultiLangSpan>
-                      </div>
-                    </>
-                  }
-                  icon={icon}
-                ></ShimmerBorderCard>
+                    }
+                    body={
+                      <>
+                        <div className="w-full text-center">
+                          <MultiLangSpan content={c.line1}></MultiLangSpan>
+                        </div>
+                        <div className="w-full text-center">
+                          <MultiLangSpan content={c.line2}></MultiLangSpan>
+                        </div>
+                      </>
+                    }
+                    icon={icon}
+                  ></ShimmerBorderCard>
+                </ScrollReveal>
               );
             })}
           </div>
@@ -99,14 +117,18 @@ export default function Contact() {
         <>
           <div className="mx-auto text-white">
             <div className="w-full mx-auto max-w-[1480px] mb-16 text-white">
-              <Heading level={2} className="font-bold">
-                <MultiLangSpan
-                  content={contactPageContent.formSection.title}
-                ></MultiLangSpan>
-              </Heading>
+              <ScrollReveal direction={"scale"} delay={Math.random()}>
+                <Heading level={2} className="font-bold">
+                  <MultiLangSpan
+                    content={contactPageContent.formSection.title}
+                  ></MultiLangSpan>
+                </Heading>
+              </ScrollReveal>
             </div>
             <div className="border-1 p-6 rounded-2xl bg-slate-700/40 border-[rgba(255,255,255,0.1)] backdrop-blur-2xl overflow-hidden shadow-[0_0_25px_6px_rgba(150,150,150,0.2)]">
-              <ContactForm></ContactForm>
+              <ScrollReveal direction={"bottom"} delay={Math.random()}>
+                <ContactForm></ContactForm>
+              </ScrollReveal>
             </div>
           </div>
         </>
